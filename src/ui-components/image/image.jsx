dd1781fdeae8fac6970683efef22171d93620838
serjo96 <king-beer@yandex.ru>
2019-12-29 03:36:26 +0500

    Added image component

---
+++ src/ui-components/image/image.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import NoImg from 'images/NoImg.png';
+import { imgUrl } from 'config';
+import Spinner from 'ui/spinner/Spinner';
+
+export default class Image extends Component {
+  get imageSrc () {
+    return this.props.src ? `${imgUrl}${this.props.src}` : NoImg;
+  }
+
+  render () {
+    const { alt = '' } = this.props;
+    return (
+      <div className='image-wrapper'>
+        {/*<Spinner />*/}
+        <img src={this.imageSrc} alt={alt} />
+      </div>
+    );
+  }
+}
