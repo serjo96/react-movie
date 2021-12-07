d5661b5cd71c8a69930586d43f48695bca3f408d
Serjo96 <king-beer@yandex.ru>
2021-12-08 01:55:30 +0300

    Added babel present ts

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,8 @@ module.exports = (api) => {
   return {
     presets: [
       '@babel/react',
-      '@babel/preset-env'
+      '@babel/preset-env',
+      '@babel/preset-typescript'
     ],
     plugins: [
       '@babel/plugin-transform-runtime',
