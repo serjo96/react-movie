4a0c9a5af78e2a3c2868c3ba4e6c97380fecf8e2
Serjo96 <king-beer@yandex.ru>
2021-12-08 01:49:59 +0300

    Fixed aliases path

--- webpack/webpack.config.babel.ts
+++ webpack/webpack.config.babel.ts
@@ -25,11 +25,12 @@ module.exports = {
     alias: {
       fonts: path.join(__dirname, '../src/assets/fonts/'),
       images: path.join(__dirname, '../src/assets/images/'),
-      utils: path.join(__dirname, '../src/utils/'),
+      utils: path.join(__dirname, '../src/utils'),
       '@ui': path.resolve(__dirname, '../src/ui-components'),
       '@templates': path.resolve(__dirname, '../src/Templates'),
       store: path.join(__dirname, '../src/store/'),
-      config: path.join(__dirname, '../src/config/')
+      config: path.join(__dirname, '../src/config/'),
+      '~': path.join(__dirname, '../src')
     }
   },
   mode: isDevelopment ? 'development' : 'production',
