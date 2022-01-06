e74f9bdf142230238724ffbc3356158af0681325
serjo96 <promlemboy@gmail.com>
2022-01-06 14:45:08 +0300

    Removed axios form general api;

--- src/store/api/general.api.ts
+++ src/store/api/general.api.ts
@@ -1,146 +1,114 @@
-import axios from 'axios';
 import { takeGenres, takeEngMedia, takeKeywordsMovies } from './../actions/general-actions';
 import { loadPlayingMovies, loadPopularMovies, loadTopMovies, loadUpcomingMovies } from './../actions/movies-actions';
 import { ThunkAction } from 'redux-thunk';
 import { AnyAction } from 'redux';
+import oldClient from '~/core/api/OldClient';
 
-export function onLoadMainPage () {
-  return (dispatch) => {
-    axios.get('https://api.themoviedb.org/3/movie/upcoming',
+export function onLoadMainPage (): ThunkAction<void, unknown, unknown, AnyAction> {
+  return dispatch => {
+    oldClient.get('movie/upcoming',
       {
-        params: {
-          api_key: '5a1d310d575e516dd3c547048eb7abf1',
-          language: 'ru-RU',
-          page: 1,
-          region: 'RU'
-        }
+        language: 'ru-RU',
+        page: 1,
+        region: 'RU'
       }
     ).then(response => {
-      dispatch(loadUpcomingMovies({ data: response.data, status: response.status === 200 }));
+      dispatch(loadUpcomingMovies({ data: response.data, status: response.status < 400 }));
     });
 
-    axios.get('https://api.themoviedb.org/3/movie/top_rated',
+    oldClient.get('movie/top_rated',
       {
-        params: {
-          api_key: '5a1d310d575e516dd3c547048eb7abf1',
-          language: 'ru-RU',
-          page: 1,
-          region: 'RU'
-        }
+        language: 'ru-RU',
+        page: 1,
+        region: 'RU'
       }
     ).then(response => {
-      dispatch(loadTopMovies({ data: response.data, status: response.status === 200 }));
+      dispatch(loadTopMovies({ data: response.data, status: response.status < 400 }));
     });
 
-    axios.get('https://api.themoviedb.org/3/movie/popular',
+    oldClient.get('movie/popular',
       {
-        params: {
-          api_key: '5a1d310d575e516dd3c547048eb7abf1',
-          language: 'ru-RU',
-          page: 1,
-          region: 'RU'
-        }
+        language: 'ru-RU',
+        page: 1,
+        region: 'RU'
       }
     ).then(response => {
-      dispatch(loadPopularMovies({ data: response.data, status: response.status === 200 }));
+      dispatch(loadPopularMovies({ data: response.data, status: response.status < 400 }));
     });
 
-    axios.get('https://api.themoviedb.org/3/movie/now_playing',
+    oldClient.get('movie/now_playing',
       {
-        params: {
-          api_key: '5a1d310d575e516dd3c547048eb7abf1',
-          language: 'ru-RU',
-          page: 1,
-          region: 'RU'
-        }
+        language: 'ru-RU',
+        page: 1,
+        region: 'RU'
       }
     ).then(response => {
-      dispatch(loadPlayingMovies({ data: response.data, status: response.status === 200 }));
+      dispatch(loadPlayingMovies({ data: response.data, status: response.status < 400 }));
     });
   };
 }
 
 export function getGenresList (): ThunkAction<void, unknown, unknown, AnyAction> {
   return async dispatch => {
-    const { genresMovie, genresTV } = await axios.all([
-      axios.get('https://api.themoviedb.org/3/genre/movie/list',
+    const [genresMovie, genresTV] = await oldClient.all([
+      oldClient.get('genre/movie/list',
         {
-          params: {
-            api_key: '5a1d310d575e516dd3c547048eb7abf1',
-            language: 'ru-RU'
-          }
+          language: 'ru-RU'
         }),
-      axios.get('https://api.themoviedb.org/3/genre/tv/list',
+      oldClient.get('genre/tv/list',
         {
-          params: {
-            api_key: '5a1d310d575e516dd3c547048eb7abf1',
-            language: 'ru-RU'
-          }
+          language: 'ru-RU'
         })
-    ]).then(axios.spread((genresMovie, genresTV) => {
-      return {
-        genresMovie,
-        genresTV
-      };
-    }));
+    ]);
     return dispatch(takeGenres({ movie: genresMovie.data.genres, tv: genresTV.data.genres }));
   };
 }
 
-export function keywordsReq (id, type, page = 1) {
-  return (dispatch) => {
-    axios.all([
-      axios.get(`https://api.themoviedb.org/3/discover/${type}`,
+export function keywordsReq (id: string, type: string, page = 1): ThunkAction<void, unknown, unknown, AnyAction> {
+  return async (dispatch) => {
+    let concatPages;
+    const [pageOne, pageTwo] = await oldClient.all([
+      oldClient.get(`https://api.themoviedb.org/3/discover/${type}`,
         {
-          params: {
-            api_key: '5a1d310d575e516dd3c547048eb7abf1',
-            language: 'ru-RU',
-            with_keywords: id,
-            page: page,
-            include_adult: true
-          }
+          language: 'ru-RU',
+          with_keywords: id,
+          page: page,
+          include_adult: true
         }),
-      axios.get(`https://api.themoviedb.org/3/discover/${type}`,
+      oldClient.get(`https://api.themoviedb.org/3/discover/${type}`,
         {
-          params: {
-            api_key: '5a1d310d575e516dd3c547048eb7abf1',
-            language: 'ru-RU',
-            with_keywords: id,
-            page: page + 1,
-            include_adult: true
-          }
+          language: 'ru-RU',
+          with_keywords: id,
+          page: page + 1,
+          include_adult: true
         })
-    ]).then(axios.spread((pageOne, pageTwo) => {
-      let concatPages;
-      if (pageOne.data.total_pages > 1) {
-        concatPages = Object.assign({
-          ...pageTwo.data,
-          results: pageOne.data.results.concat(pageTwo.data.results),
-          page: pageOne.data.page,
-          searchType: { type: 'genres' }
-        });
-      } else {
-        concatPages = pageOne.data;
-      }
-      dispatch(takeKeywordsMovies({ data: concatPages, status: { pageOne: pageOne.status === 200, pageTwo: pageTwo.status === 200 } }));
-    }));
+    ]);
+
+    if (pageOne.data.total_pages > 1) {
+      concatPages = Object.assign({
+        ...pageTwo.data,
+        results: pageOne.data.results.concat(pageTwo.data.results),
+        page: pageOne.data.page,
+        searchType: { type: 'genres' }
+      });
+    } else {
+      concatPages = pageOne.data;
+    }
+
+    dispatch(takeKeywordsMovies({ data: concatPages, status: { pageOne: pageOne.status < 400, pageTwo: pageTwo.status < 400 } }));
   };
 }
 
-export function onLoadEngMedia (id, type) {
-  return (dispatch) => {
-    axios.get(`https://api.themoviedb.org/3/${type}/${id}`,
+export function onLoadEngMedia (id: string, type: string): ThunkAction<void, unknown, unknown, AnyAction> {
+  return async (dispatch) => {
+    const { data } = await oldClient.get(`${type}/${id}`,
       {
-        params: {
-          api_key: '5a1d310d575e516dd3c547048eb7abf1',
-          language: 'en-US',
-          include_image_language: 'ru,null',
-          append_to_response: 'credits,images,videos,recommendations,reviews,lists,keywords,release_dates'
-        }
+        language: 'en-US',
+        include_image_language: 'ru,null',
+        append_to_response: 'credits,images,videos,recommendations,reviews,lists,keywords,release_dates'
       }
-    ).then(res => {
-      const response = Object.assign({ ...res.data, typeResponse: type });
-      dispatch(takeEngMedia(response));
-    });
+    );
+    const response = Object.assign({ ...data, typeResponse: type });
+    dispatch(takeEngMedia(response));
   };
 }
