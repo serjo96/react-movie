731bfa33ce1a83fe5bf11017a74ebf39901cb140
serjo96 <promlemboy@gmail.com>
2022-01-15 00:46:23 +0300

    Added search and keywords reducers;

--- src/store/Reducers/mainReducer.ts
+++ src/store/Reducers/mainReducer.ts
@@ -1,14 +1,18 @@
 import { combineReducers } from 'redux';
-import General from './generalReducer.ts';
+import General from './generalReducer';
 import Movies from './movieReducers';
 import TVs from './tvReducers';
 import Peoples from './personReducer';
 import Company from './companyReducer';
+import Search from './searchReducer';
+import { keywordsSlice } from '~/store/keywords/keywords.slice';
 
 export default combineReducers({
+  [keywordsSlice.name]: keywordsSlice.reducer,
   General,
   Movies,
   TVs,
   Peoples,
-  Company
+  Company,
+  Search
 });
