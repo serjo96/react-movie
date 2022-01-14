4496d9c72b78f74e4f6c58c1ecb5b80e1b102d51
serjo96 <promlemboy@gmail.com>
2022-01-15 00:43:28 +0300

    Added ts for ServiceBlock.tsx

--- src/templates/Service/ServiceBlock.tsx
+++ src/templates/Service/ServiceBlock.tsx
@@ -2,19 +2,26 @@ import React from 'react';
 import Spinner from '~/ui-components/spinner/Spinner';
 import TryAgain from '../TryAgain/TryAgain';
 
+interface MyProps {
+  children: React.ReactNode;
+  isLoading: boolean;
+  isError: boolean;
+  fetch: () => void;
+}
+
 const ServiceBlock = ({
   children,
   isLoading,
   isError,
   fetch
-}) => {
-  const showPreloader = !isLoading && isError;
-  const allDataIsReady = isLoading && isError;
+}: MyProps) => {
+  const showPreloader = isLoading && !isError;
+  const allDataIsReady = !isLoading && !isError;
 
   return (
     <div>
       {showPreloader && <Spinner />}
-      {!isError && <TryAgain fetch={fetch} />}
+      {isError && <TryAgain fetch={fetch} />}
       {allDataIsReady && children}
     </div>
   );
