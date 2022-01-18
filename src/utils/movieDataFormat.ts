8d15388919879e090c3370fe814a05dbfdef691d
serjo96 <promlemboy@gmail.com>
2022-01-19 00:18:17 +0300

    Fixed arg type from string to number;

--- src/utils/movieDataFormat.ts
+++ src/utils/movieDataFormat.ts
@@ -1,3 +1,3 @@
-export function movieDues (dues: string) {
+export function movieDues (dues: number) {
   return dues ? '$' + dues.toString().replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g, '\$1 ') : '-';
 }
