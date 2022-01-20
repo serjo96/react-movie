8168278414e21f11a5792520be05dc3527c16e3e
serjo96 <promlemboy@gmail.com>
2022-01-20 17:29:40 +0300

    Removed images alias;

--- webpack/webpack.resolve.ts
+++ webpack/webpack.resolve.ts
@@ -5,7 +5,6 @@ const resolve: webpack.ResolveOptions = {
   extensions: ['.js', '.ts', '.jsx', '.tsx'],
   alias: {
     fonts: path.join(__dirname, '../src/assets/fonts/'),
-    images: path.join(__dirname, '../src/assets/images/'),
     '~': path.join(__dirname, '../src')
   }
 };
