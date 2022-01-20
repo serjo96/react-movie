392d11be549f1ae289d56a5eaff2187801a27bd0
serjo96 <promlemboy@gmail.com>
2022-01-21 02:43:30 +0300

    Added types and functional component for movie collections;

---
+++ src/templates/Movie/components/movie-collection.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { MovieItem } from '~/templates/moviesList/components';
+import { Collection } from '~/core/types/collection';
+import './movie-collection.sass';
+
+const MovieCollection = ({ collection }: {collection: Collection}) => {
+  const randomBackgroundImg = 'url(https://image.tmdb.org/t/p/original' + (Math.random() < 0.5
+    ? (collection.backdropPath || collection.posterPath)
+    : (collection.posterPath || collection.backdropPath)) + ')';
+
+  return (
+    <section
+      className='collection'
+      style={{
+        backgroundImage: randomBackgroundImg
+      }}
+    >
+      <div className='bg-base' />
+      <div className='collection__data'>
+        <div className='container'>
+          <h3 className='collection__title'>{collection.name}</h3>
+          <p className='collection__overview'>{collection.overview}</p>
+          <div className='collection__list tooltip-parent'>
+            {collection.parts.map((el, index) => (
+              <MovieItem
+                title={el.title}
+                original_title={el.originalTitle}
+                overview={el.overview}
+                voteAverage={el.voteAverage}
+                date={el.releaseDate}
+                poster={el.posterPath}
+                id={el.id}
+                key={index}
+                genres={el.genreIds}
+                typeList='movie'
+              />)
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default (MovieCollection);
