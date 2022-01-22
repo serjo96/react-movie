0b69709b9ba12e57a4c1700baab619ac1b936190
serjo96 <promlemboy@gmail.com>
2022-01-23 02:41:26 +0300

    Added styles alias for sass;

--- webpack/webpack.resolve.ts
+++ webpack/webpack.resolve.ts
@@ -5,6 +5,7 @@ const resolve: webpack.ResolveOptions = {
   extensions: ['.js', '.ts', '.jsx', '.tsx'],
   alias: {
     fonts: path.join(__dirname, '../src/assets/fonts/'),
+    styles: path.join(__dirname, '../src/styles/'),
     '~': path.join(__dirname, '../src')
   }
 };
