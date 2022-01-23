6f6a783b778e539213b18b07d560f64ac1a3b233
serjo96 <promlemboy@gmail.com>
2022-01-23 17:40:07 +0300

    Added functional component;

--- src/templates/Movie/containers/movie.tsx
+++ src/templates/Movie/containers/movie.tsx
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { RouteComponentProps } from 'react-router';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { useParams } from 'react-router-dom';
 
-import { AppDispatch, RootState } from '~/store/configureStore';
 import { Languages } from '~/store/Reducers/generalReducer';
+import { getMovieData, getMovieEngOverview } from '~/store/movies/movies.api';
 import MovieAside from './../components/movie-aside';
 import MediaStills from '~/templates/media-page/media-stills';
 import MediaCast from '~/templates//media-page/media-cast';
-import {getMovieData, getMovieEngOverview} from '~/store/movies/movies.api';
 import MediaTop from '~/templates/media-page/media-top';
 import { MovieSummary } from '~/templates/Movie/components/movie-summary';
 import ServiceBlock from '~/templates/Service/ServiceBlock';
@@ -18,140 +15,109 @@ import MovieCollection from '~/templates/Movie/components/movie-collection';
 import MovieDescription from '~/ui-components/MovieDescription/MovieDescription';
 import { VideosSection } from '~/ui-components/video-section/videos-section';
 import { MediaType } from '~/core/types/media-type';
+import { useAppDispatch, useAppSelector } from '~/hooks/storeHooks';
 
-interface MyState {
-  imgCount: number,
-  imgStatus: boolean,
-}
+function Movie () {
+  const appDispatch = useAppDispatch();
+  const { id } = useParams<{id: string}>();
+  const [prevProps] = useState(id);
+  const { isFetching, isSuccessful, data } = useAppSelector(state => state.movies);
+  const movie = data;
 
-interface MyProps extends RouteComponentProps<{id?: string}>{
-  movie: RootState['movies']['data'];
-  isFetching: boolean;
-  isSuccessful: boolean;
-  getMovieData: typeof getMovieData;
-  getMovieEngData: typeof getMovieEngOverview;
-}
-
-class Movie extends Component<MyProps, MyState> {
-  state = {
-    imgCount: 11,
-    imgStatus: true
+  const sendRequest = () => {
+    const movieId = id.split('-').pop();
+    appDispatch(getMovieData({ id: movieId }));
   };
-
-  componentDidMount () {
-    this.sendRequest();
-  }
-
-  componentWillReceiveProps (nextProps: MyProps) {
-    if (nextProps.match.params.id !== this.props.match.params.id) {
-      this.sendRequest(nextProps.match.params.id);
-      this.scrollToTop();
-    }
-  }
-
-  scrollToTop = () => {
+  const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
+  useEffect(() => {
+    if (!isFetching) {
+      sendRequest();
+    }
 
-  sendRequest = (id = this.props.match.params.id) => {
-    const movieId = id.split('-').pop();
-    this.props.getMovieData({ id: movieId });
+    if (id !== prevProps) {
+      sendRequest();
+      scrollToTop();
+    }
+  }, [id]);
+
+  const handlerOnFetchEngData = () => {
+    appDispatch(getMovieEngOverview({ id: `${this.props.movie.id}`, lang: Languages.EN }));
   };
 
-  handlerOnFetchEngData = () => {
-    this.props.getMovieEngData({ id: `${this.props.movie.id}`, lang: Languages.EN });
-  };
+  return (
+    <ServiceBlock
+      isLoading={isFetching}
+      isError={!isSuccessful}
+      fetch={sendRequest}
+    >
+      <div className='movie'>
+        <Helmet>
+          <title>{movie.title}</title>
+        </Helmet>
 
-  render () {
-    const movie = this.props.movie;
-    const { isSuccessful, isFetching } = this.props;
+        <MediaTop
+          title={movie.title}
+          originalTitle={movie.originalTitle}
+          backdrop={movie.backdropPath}
+          poster={movie.posterPath}
+          releaseDate={movie.releaseDate}
+          tagline={movie.tagline}
+        >
+          <MovieSummary />
+        </MediaTop>
 
-    return (
-      <ServiceBlock
-        isLoading={isFetching}
-        isError={!isSuccessful}
-        fetch={this.sendRequest}
-      >
-        <div className='movie'>
-          <Helmet>
-            <title>{movie.title}</title>
-          </Helmet>
+        <div className='container'>
+          <div className='info-wrapper'>
 
-          <MediaTop
-            title={movie.title}
-            originalTitle={movie.originalTitle}
-            backdrop={movie.backdropPath}
-            poster={movie.posterPath}
-            releaseDate={movie.releaseDate}
-            tagline={movie.tagline}
-          >
-            <MovieSummary />
-          </MediaTop>
+            <MovieAside
+              id={movie.id}
+              poster={movie.posterPath}
+              backdrop={movie.backdropPath}
+              crew={movie.credits.crew}
+              genres={movie.genres}
+              keywords={movie.keywords.keywords}
+              imdbId={movie.imdbId}
+              homepage={movie.homepage}
+              productionCountries={movie.productionCountries}
+              productionCompanies={movie.productionCompanies}
+            />
 
-          <div className='container'>
-            <div className='info-wrapper'>
-
-              <MovieAside
-                id={movie.id}
-                poster={movie.posterPath}
-                backdrop={movie.backdropPath}
-                crew={movie.credits.crew}
-                genres={movie.genres}
-                keywords={movie.keywords.keywords}
-                imdbId={movie.imdbId}
-                homepage={movie.homepage}
-                productionCountries={movie.productionCountries}
-                productionCompanies={movie.productionCompanies}
+            <div className='overview'>
+              <MovieDescription
+                overview={movie.overview}
+                fetchEngData={handlerOnFetchEngData}
               />
 
-              <div className='overview'>
-                <MovieDescription
-                  overview={movie.overview}
-                  fetchEngData={this.handlerOnFetchEngData}
-                />
+              <VideosSection videos={movie.videos} />
 
-                <VideosSection videos={movie.videos} />
+              <MediaCast cast={movie.credits.cast} />
+              <MediaStills
+                images={movie.images.backdrops}
+                title='Кадры из фильма'
+                imgCount={16}
+              />
 
-                <MediaCast cast={movie.credits.cast} />
-                <MediaStills
-                  images={movie.images.backdrops}
-                  title='Кадры из фильма'
-                  imgCount={16}
-                />
-
-                <MediaStills
-                  images={movie.images.posters}
-                  title='Постеры'
-                  posters
-                  imgCount={8}
-                />
-              </div>
+              <MediaStills
+                images={movie.images.posters}
+                title='Постеры'
+                posters
+                imgCount={8}
+              />
             </div>
           </div>
-
-          <MovieCollection collection={movie.collection} />
-          <MediaRecommendations
-            recommendations={movie.recommendations}
-            listName='Вам может понравиться'
-            typeList={MediaType.MOVIE}
-          />
         </div>
-      </ServiceBlock>
-    );
-  }
+
+        <MovieCollection collection={movie.collection} />
+        <MediaRecommendations
+          recommendations={movie.recommendations}
+          listName='Вам может понравиться'
+          typeList={MediaType.MOVIE}
+        />
+      </div>
+    </ServiceBlock>
+  );
 }
 
-function mapStateToProps (state: RootState) {
-  return {
-    movie: state.movies.data,
-    isSuccessful: state.movies.isSuccessful,
-    isFetching: state.movies.isFetching
-  };
-}
-
-const mapDispatchToProps = (dispatch: AppDispatch) => ({
-  getMovieData: bindActionCreators(getMovieData, dispatch),
-  getMovieEngData: bindActionCreators(getMovieEngOverview, dispatch)
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Movie);
+export default Movie;
