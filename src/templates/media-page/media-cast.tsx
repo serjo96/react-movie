ddff626c6f04d8e2f47dfca1aca6ad415221ee44
serjo96 <promlemboy@gmail.com>
2022-01-23 15:05:36 +0300

    Added swiper instead of react-id-swiper;

--- src/templates/media-page/media-cast.tsx
+++ src/templates/media-page/media-cast.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Swiper from 'react-id-swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {
+  Scrollbar
+} from 'swiper';
 
 import { friendlyUrl } from '~/utils/format';
 import { Cast } from '~/core/types/cast';
 import NoImg from '~/assets/images/noImg.png';
 import './media-cast.sass';
+
+SwiperCore.use([Scrollbar]);
 
 const MediaCast = ({ cast }: {cast: Cast[]}) => {
   if (!cast.length) {
@@ -34,31 +39,33 @@ const MediaCast = ({ cast }: {cast: Cast[]}) => {
               spaceBetween: 0
             }
           }}
-          freeMode
           scrollbar={{
             el: '.swiper-scrollbar',
             hide: false,
             draggable: true
           }}
-          shouldSwiperUpdate
-          mousewheel={cast.length > 7 ? { sensitivity: 150 } : false}
+          centeredSlides={false}
+          mousewheel={cast.length > 7 && { sensitivity: 150 }}
         >
           {cast.map((actor, indx) => (
-            <Link
-              className='actor'
-              to={`/person/${friendlyUrl(actor.name)}-${actor.id}`}
-              key={indx}
-            >
-              <div
-                className='actor__img'
-                style={{ backgroundImage: actor.profilePath ? `url(https://image.tmdb.org/t/p/w185/${actor.profilePath})` : `url(${NoImg})` }}
-              />
-              <div className='actor__info'>
-                <div className='actor__name'>{actor.name}</div>
-                {actor.character &&
-                  <div className='actor__role'>{actor.character}</div>}
-              </div>
-            </Link>)
+            <SwiperSlide key={indx}>
+              <Link
+                className='actor'
+                to={`/person/${friendlyUrl(actor.name)}-${actor.id}`}
+
+              >
+                <div
+                  className='actor__img'
+                  style={{ backgroundImage: actor.profilePath ? `url(https://image.tmdb.org/t/p/w185/${actor.profilePath})` : `url(${NoImg})` }}
+                />
+                <div className='actor__info'>
+                  <div className='actor__name'>{actor.name}</div>
+                  {actor.character &&
+                    <div className='actor__role'>{actor.character}</div>}
+                </div>
+              </Link>
+            </SwiperSlide>
+          )
           )}
         </Swiper>
 
