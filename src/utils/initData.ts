5dc3af23847f71419781df24d17dd9eed9130db5
serjo96 <promlemboy@gmail.com>
2022-02-01 02:09:32 +0300

    Added init data functions for tv page;

--- src/utils/initData.ts
+++ src/utils/initData.ts
@@ -1,13 +1,45 @@
 import ActionPayloadData from '~/core/types/actionPayloadData';
 import { ListData } from '~/core/types/listData';
+import { Cast } from '~/core/types/cast';
+import { Crew } from '~/core/types/crew';
+import { Images } from '~/core/types/images';
+
+export type CrewState = {
+  director: Crew[],
+  screenplay: Crew[],
+  producer: Crew[],
+  directorOfPhotography: Crew[],
+  music: Crew[],
+  art: Crew[]
+}
+
+export const initDataState = <T>(): ListData<T> => ({
+  page: 1,
+  results: [],
+  totalResults: 0,
+  totalPages: 0
+});
 
 export const initListData = <T>(): ActionPayloadData<ListData<T>> => ({
   isFetching: false,
   isSuccess: true,
-  data: {
-    page: 1,
-    results: [],
-    totalResults: 0,
-    totalPages: 0
+  data: initDataState<T>()
+});
+
+export const initCreditsState = (): {crew: CrewState} & {cast: Cast[]} => ({
+  cast: [],
+  crew: {
+    director: [],
+    screenplay: [],
+    producer: [],
+    directorOfPhotography: [],
+    music: [],
+    art: []
   }
+});
+
+export const initImagesState = (): Images => ({
+  backdrops: [],
+  posters: [],
+  logos: []
 });
