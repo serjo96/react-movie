a8dcc946ecc31b1558099ea8c441d14ce04c3ae1
serjo96 <promlemboy@gmail.com>
2022-02-02 03:25:18 +0300

    Added search api;

---
+++ src/store/search/search.api.ts
@@ -0,0 +1,72 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { ListData } from '~/core/types/listData';
+import { SearchResultItem } from '~/core/types/search';
+import oldClient from '~/core/api/OldClient';
+import ConcatPages from '~/utils/concatPages';
+
+export type SearchResponse = {
+  isSuccessful: boolean;
+  data: ListData<SearchResultItem>
+};
+
+type SearchPayload = {
+  words: string;
+  page: number;
+  adult?: boolean;
+}
+
+export const getSearchData = createAsyncThunk<SearchResponse, string>(
+  'search/getSearchData',
+  async (words) => {
+    const { data, isSuccessRequest } = await oldClient.get('search/multi',
+      {
+        language: 'ru-RU',
+        page: 1,
+        region: 'RU',
+        query: words,
+        include_adult: true
+      }
+    );
+    return {
+      data,
+      isSuccessful: isSuccessRequest
+    };
+  });
+
+export const getMainSearchData = createAsyncThunk<SearchResponse, SearchPayload>(
+  'search/getMainSearchData',
+  async ({
+    words,
+    page = 1,
+    adult = false
+  }: SearchPayload = {
+    page: 1,
+    words: '',
+    adult: false
+  }) => {
+    const [firstPage, secondPage] = await oldClient.all<SearchResponse['data']>([
+      oldClient.get('search/multi',
+        {
+          language: 'ru-RU',
+          page: page,
+          region: 'RU',
+          include_adult: adult,
+          query: words.replace('_', ' ')
+        }),
+      oldClient.get('search/multi',
+        {
+          language: 'ru-RU',
+          page: page + 1,
+          region: 'RU',
+          include_adult: adult,
+          query: words.replace('_', ' ')
+        })
+    ]);
+    const concatPages = ConcatPages<SearchResultItem>({ firstPage, secondPage });
+
+    return {
+      data: concatPages,
+      isSuccessful: firstPage.isSuccessRequest && secondPage.isSuccessRequest
+    };
+  });
