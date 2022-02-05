0028ccf48690428b16fb6ce4637994b540141793
serjo96 <promlemboy@gmail.com>
2022-02-06 01:17:58 +0300

    Added search reducer;

--- src/store/Reducers/mainReducer.ts
+++ src/store/Reducers/mainReducer.ts
@@ -4,21 +4,21 @@ import Movies from './movieReducers';
 import TVs from './tvReducers';
 import Peoples from './personReducer';
 import Company from './companyReducer';
-import Search from './searchReducer';
 import { keywordsSlice } from '~/store/keywords/keywords.slice';
 import { moviesSlice } from '~/store/movies/movies.slice';
 import { genresSlice } from '~/store/genres/genres.slice';
 import { tvSlice } from '~/store/tv/tv.slice';
+import { searchSlice } from '~/store/search/search.slice';
 
 export default combineReducers({
   keywords: keywordsSlice.reducer,
   movies: moviesSlice.reducer,
   genres: genresSlice.reducer,
   tvShows: tvSlice.reducer,
+  search: searchSlice.reducer,
   General,
   Movies,
   TVs,
   Peoples,
-  Company,
-  Search
+  Company
 });
