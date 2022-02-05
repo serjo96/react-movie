4c7ee3deab02d0f24a16aa66633ed166a27e42eb
serjo96 <promlemboy@gmail.com>
2022-02-05 22:51:57 +0300

    Added spinner for image component;

--- src/ui-components/image/image.tsx
+++ src/ui-components/image/image.tsx
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import NoImg from '~/assets/images/noImg.png';
 import { imgUrl } from '~/core/config';
 import Spinner from '~/ui-components/spinner/Spinner';
+import classNames from 'classnames';
+import './image.sass';
 
 interface MyProps {
   src?: string | null;
   alt?: string;
+  showSpinner?: boolean;
 }
 
-export default class Image extends Component<MyProps> {
-  get imageSrc () {
-    return this.props.src ? `${imgUrl}${this.props.src}` : NoImg;
-  }
+export default function Image (props: MyProps) {
+  const [imgStatus, setImgStatus] = useState(true);
+  const imageSrc = props.src ? `${imgUrl}${props.src}` : NoImg;
+  const onLoadImg = () => {
+    setTimeout(() => setImgStatus(false), 500);
+  };
+  const imgClass = classNames({ 'img-loading': imgStatus });
 
-  render () {
-    const { alt = '' } = this.props;
-    return (
-      <div className='image-wrapper'>
-        {/* <Spinner /> */}
-        <img src={this.imageSrc} alt={alt} />
-      </div>
-    );
-  }
+  const { alt = '' } = props;
+  return (
+    <div className='image-wrapper'>
+      {(props.showSpinner && imgStatus) && <Spinner />}
+      <img
+        className={imgClass}
+        src={imageSrc}
+        alt={alt}
+        onLoad={onLoadImg}
+      />
+    </div>
+  );
 }
