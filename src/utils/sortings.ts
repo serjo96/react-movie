b837c1fb50de9f4a8f2ad52bece2b8f09f670b40
serjo96 <promlemboy@gmail.com>
2022-02-09 02:58:12 +0300

    Added sorting sortMoviesItems,sortTvShows, best media by count and average;

--- src/utils/sortings.ts
+++ src/utils/sortings.ts
@@ -1,4 +1,7 @@
 import { Collection } from '~/core/types/collection';
+import { MovieCreditsCast, PersonCrew } from '~/core/types/perosn-details';
+import { MoviesListItem } from '~/core/types/movies';
+import { TvListItem } from '~/core/types/tv';
 
 export function sortCollectionItems (collection: Collection['parts']) {
   return [...collection].sort((a, b) => {
@@ -8,3 +11,15 @@ export function sortCollectionItems (collection: Collection['parts']) {
     return new Date(a.releaseDate) < new Date(b.releaseDate) ? -1 : 1;
   });
 }
+
+export function sortMoviesItems (collection: MovieCreditsCast[]) {
+  return [...collection].sort((a, b) => {
+    return +new Date(a.releaseDate) - +new Date(b.releaseDate);
+  });
+}
+
+export function sortTvShows (collection: Array<PersonCrew>) {
+  return [...collection].sort((a, b) => +new Date(a.firstAirDate) - +new Date(b.firstAirDate));
+}
+
+export const sortBestMediaItem = (movies: Array<MovieCreditsCast | PersonCrew | TvListItem | MoviesListItem>) => [...movies].sort((a, b) => b.voteCount - a.voteCount || b.voteAverage - a.voteAverage);
