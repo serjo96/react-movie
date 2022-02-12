42a684cae314618630b57c4983db3e3b0390e608
serjo96 <promlemboy@gmail.com>
2022-02-12 15:06:23 +0300

    Added type for toggle func;

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,10 +1,12 @@
 import { useCallback, useState } from 'react';
 
-export const useToggle = (initialState = false): [boolean, any] => {
+type ToggleFuncType = () => void;
+
+export const useToggle = (initialState = false): [boolean, ToggleFuncType] => {
   // Initialize the state
   const [state, setState] = useState<boolean>(initialState);
   // Define and memorize toggler function in case we pass down the comopnent,
   // This function change the boolean value to it's opposite value
-  const toggle = useCallback((): void => setState(state => !state), []);
+  const toggle = useCallback<ToggleFuncType>((): void => setState(state => !state), []);
   return [state, toggle];
 };
