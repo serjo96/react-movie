d55614de1127af2c860edbfea906303c2d7dff02
serjo96 <promlemboy@gmail.com>
2022-02-12 15:06:49 +0300

    Moved nav to header component;

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React, { Component, useEffect } from 'react';
-import { useHistory, useLocation, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Routes from './Routes/Routes';
 
-import Header from './templates/Head/Head';
-import Nav from './templates/Nav/nav';
+import Header from './templates/head/head';
 import { useAppDispatch } from '~/hooks/storeHooks';
 import { getGenres } from '~/store/genres/generes.api';
 import './../styles/main.sass';
 
 function App () {
   const appDispatch = useAppDispatch();
-  const history = useHistory();
-  const location = useLocation();
 
   const genresInLocalStorage = Boolean(JSON.parse(localStorage.getItem('genres')));
   useEffect(() => {
@@ -27,8 +24,7 @@ function App () {
         <title>Movie Base</title>
       </Helmet>
       <React.StrictMode>
-        <Nav location={location} />
-        <Header history={history} />
+        <Header />
         <Routes />
       </React.StrictMode>
     </React.Fragment>
