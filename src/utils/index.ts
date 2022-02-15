b10ab9a4e22232feef81c2d1380822334eb491d0
serjo96 <promlemboy@gmail.com>
2022-02-16 02:11:59 +0300

    Removed unused helpers;

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,3 @@
-export function isMatching (full, chunk) {
-  full = full.toLowerCase();
-  chunk = chunk.toLowerCase();
-  return full.substr(0, chunk.length) === chunk;
-}
-
-export function sortSeasons (a, b) {
-  if (a.season_number === 0) return 1;
-  if (b.season_number === 0) return -1;
-  if (new Date(a.season_number) === new Date(b.season_number)) return 0;
-  return new Date(a.season_number) < new Date(b.season_number) ? -1 : 1;
-}
-
 export const chunkArr = <T>(arr: T[], chunkSize: number) => {
   const groups = [];
   for (let i = 0; i < arr.length; i += chunkSize) {
