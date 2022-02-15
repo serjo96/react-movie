99f4c89978ccd6f3d974c1201aa8f5e27e74e2d6
serjo96 <promlemboy@gmail.com>
2022-02-15 20:41:33 +0300

    ADded configure store;

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,6 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import rootReducer from './Reducers/mainReducer';
+import { configureStore, Store } from '@reduxjs/toolkit';
+import { rootReducer } from './mainReducer';
 
 const store = configureStore({
   reducer: rootReducer,
