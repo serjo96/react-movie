262bb063f30d850ee053a8389c5ff7a80ac64818
serjo96 <promlemboy@gmail.com>
2022-03-17 01:52:57 +0300

    Added init 18n plugin

---
+++ src/i18n.ts
@@ -0,0 +1,57 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { Languages } from '~/store/config/config.slice';
+
+import moviesList from '../public/locales/en/lists/list.json';
+import COMMON_EN from '../public/locales/en/common/common.json';
+import COMMON_RU from '../public/locales/ru/common/common.json';
+import oldClient from '~/core/api/OldClient';
+
+export const defaultNS = 'common';
+export const resources = {
+  en: {
+    // movie,
+    // tv,
+    common: COMMON_EN,
+    moviesList
+  },
+  ru: {
+    // movie,
+    // tv,
+    common: COMMON_RU
+    // moviesList
+  }
+} as const;
+
+i18n.on('initialized', () => {
+  oldClient.setQueryParams({ language: i18n.language }, false);
+});
+
+i18n.on('languageChanged', (language: Languages) => {
+  oldClient.setQueryParams({ language }, false);
+});
+
+i18n
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    defaultNS,
+    fallbackLng: Languages.EN,
+    debug: true,
+    react: {
+      useSuspense: false //   <---- this will do the magic
+    },
+    interpolation: {
+      escapeValue: false // not needed for react as it escapes by default
+    },
+    resources
+  });
+
+export default i18n;
