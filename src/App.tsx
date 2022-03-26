9f8fbac4dd24fd6a3a83f8718e4a81d832140a32
serjo96 <promlemboy@gmail.com>
2022-03-26 21:32:21 +0300

    Added resend request after change lang;

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,20 @@ import Header from './templates/head/head';
 import { useAppDispatch } from '~/hooks/storeHooks';
 import { getGenres } from '~/store/genres/generes.api';
 import './../styles/main.sass';
+import useTranslations from '~/hooks/useTranslations';
+import { usePrevious } from '~/hooks/usePrevious';
 
 function App () {
   const appDispatch = useAppDispatch();
+  const { lang } = useTranslations();
+  const prevLang = usePrevious(lang);
 
   const genresInLocalStorage = Boolean(JSON.parse(localStorage.getItem('genres')));
   useEffect(() => {
-    if (!genresInLocalStorage) {
+    if (!genresInLocalStorage || lang !== prevLang) {
       appDispatch(getGenres());
     }
-  }, []);
+  }, [lang]);
   // TODO: Add title component, with generation title
   return (
     <React.Fragment>
