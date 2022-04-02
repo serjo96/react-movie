ddfdf56abb10f43e21e0b5487556bf69ccd15e98
serjo96 <promlemboy@gmail.com>
2022-04-03 02:16:01 +0300

    Added unknown type;

--- src/hooks/useLangEffect.ts
+++ src/hooks/useLangEffect.ts
@@ -2,7 +2,7 @@ import { EffectCallback, useEffect } from 'react';
 
 import useTranslations from '~/hooks/useTranslations';
 
-export const useLangEffect = <T>(func: EffectCallback, deps?: Array<string | boolean | number>) => {
+export const useLangEffect = <T>(func: EffectCallback, deps?: Array<string | boolean | number | unknown>) => {
   const { lang } = useTranslations();
   const deepsArr = deps ? [lang, ...deps] : undefined;
 
