a7704da879600db8e9b79ede8a665894c22749c0
serjo96 <promlemboy@gmail.com>
2022-04-13 02:37:38 +0300

    Added tippyjs instead of nattive css

--- src/ui-components/language-switcher/language-switcher.tsx
+++ src/ui-components/language-switcher/language-switcher.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Languages } from '~/store/config/config.slice';
 import './language-switcher.sass';
 import useTranslations from '~/hooks/useTranslations';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
 
 export default function LanguageSwitcher () {
   const { setLang, lang } = useTranslations();
@@ -11,27 +13,34 @@ export default function LanguageSwitcher () {
   return (
 
     <div className='language-switcher'>
-      <button className='language-switcher__button link'>
-        <span>{selectedLanguageTitle}</span>
-        <i className='fa fa-sort-desc' />
-      </button>
+      <Tippy
+        interactive
+        className='languages-list'
+        content={
+          <React.Fragment>
+            <button
+              disabled={lang === Languages.EN}
+              className='languages-list__item'
+              onClick={() => setLang(Languages.EN)}
+            >
+            ENG
+            </button>
+            <button
+              disabled={lang === Languages.RU}
+              className='languages-list__item'
+              onClick={() => setLang(Languages.RU)}
+            >
+            RU
+            </button>
+          </React.Fragment>
+        }
+      >
+        <button className='language-switcher__button link'>
+          <span>{selectedLanguageTitle}</span>
+          <i className='fa fa-sort-desc' />
+        </button>
+      </Tippy>
 
-      <div className='languages-list'>
-        <button
-          disabled={lang === Languages.EN}
-          className='languages-list__item'
-          onClick={() => setLang(Languages.EN)}
-        >
-          ENG
-        </button>
-        <button
-          disabled={lang === Languages.RU}
-          className='languages-list__item'
-          onClick={() => setLang(Languages.RU)}
-        >
-          RU
-        </button>
-      </div>
     </div>
 
   );
