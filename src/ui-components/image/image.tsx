5cd1dc85dc03b87812533d9d8b5580ed7ee5d126
serjo96 <promlemboy@gmail.com>
2022-04-23 00:19:57 +0300

    Added image styles for error;

--- src/ui-components/image/image.tsx
+++ src/ui-components/image/image.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import NoImg from '~/assets/images/noImg.png';
+import errorImg from '~/assets/images/error.png';
 import { imgUrl } from '~/core/config';
 import Spinner from '~/ui-components/spinner/Spinner';
 import classNames from 'classnames';
@@ -15,25 +16,39 @@ interface MyProps {
 
 export default function Image (props: MyProps) {
   const [imgStatus, setImgStatus] = useState(true);
+  const [imgError, setImgError] = useState(false);
   let imageSrc = NoImg;
 
   if (props.src) {
     imageSrc = props.src.match(/^http/) ? props.src : `${imgUrl}${props.src}`;
   }
 
+  if (imgError) {
+    imageSrc = errorImg;
+  }
+
   const onLoadImg = () => {
     setTimeout(() => setImgStatus(false), 500);
   };
+
+  const onErrorImg = () => {
+    setImgError(true);
+  };
   const imgClass = classNames(props.className, { 'img-loading': imgStatus });
+  const imgWrapperClass = classNames('image-wrapper', {
+    'image-wrapper--preload': imgStatus || imgError,
+    'image-wrapper--error': imgError
+  });
 
   const { alt = '' } = props;
   return (
-    <div className='image-wrapper'>
-      {(props.showSpinner && imgStatus) && <Spinner />}
+    <div className={imgWrapperClass}>
+      {(props.showSpinner && imgStatus && !imgError) && <Spinner />}
       <img
         className={imgClass}
         src={imageSrc}
         alt={alt}
+        onError={onErrorImg}
         onLoad={onLoadImg}
       />
     </div>
