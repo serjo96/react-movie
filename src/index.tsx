c85fe4f5ef36a24aac3e24ee4b0fed17c0bc1cc6
serjo96 <promlemboy@gmail.com>
2022-04-24 20:56:46 +0300

    Added env for sentry

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import store from './store/configureStore';
 Sentry.init({
   dsn: 'https://ce35249271eb438b941026c6779e2381@o1215286.ingest.sentry.io/6356317',
   integrations: [new BrowserTracing()],
-
+  environment: process.env.NODE_ENV,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
