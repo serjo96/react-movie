ba6c782c7ec6ca2fe0c498d120e0e4c76e784ea0
serjo96 <promlemboy@gmail.com>
2022-04-29 20:51:45 +0300

    Added condition for sentry only for prod;

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,17 @@ import App from './App';
 import './i18n';
 import store from './store/configureStore';
 
-Sentry.init({
-  dsn: 'https://ce35249271eb438b941026c6779e2381@o1215286.ingest.sentry.io/6356317',
-  integrations: [new BrowserTracing()],
-  environment: process.env.NODE_ENV,
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://ce35249271eb438b941026c6779e2381@o1215286.ingest.sentry.io/6356317',
+    integrations: [new BrowserTracing()],
+    environment: process.env.NODE_ENV,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0
+  });
+}
 
 render(
   <Provider store={store}>
