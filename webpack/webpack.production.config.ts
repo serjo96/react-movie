27432979d4d41df8c95869f060bc417ea9fc9aa4
serjo96 <promlemboy@gmail.com>
2022-05-06 12:43:49 +0300

    Added test repo name;

--- webpack/webpack.production.config.ts
+++ webpack/webpack.production.config.ts
@@ -72,7 +72,9 @@ const exportConfig: webpack.Configuration = {
         name: process.env.BUILD_ID
       },
       setCommits: {
-        auto: true
+        repo: 'serjo96/react-movie',
+        commit: process.env.COMMIT_REF,
+        previousCommit: process.env.previousCommit
       }
     }),
     ...config.plugins,
