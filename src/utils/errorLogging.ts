4046c6f272867c78a383348debed7e6025477c11
serjo96 <promlemboy@gmail.com>
2022-05-06 12:18:01 +0300

    Added toast error notice;

--- src/utils/errorLogging.ts
+++ src/utils/errorLogging.ts
@@ -1,7 +1,10 @@
 import * as Sentry from '@sentry/react';
+import { toast } from 'react-toastify';
+import i18n from '~/i18n';
 import { formattedThunkError } from '~/utils/formatThunkErrorPayload';
 
 export default function errorLogging (error: formattedThunkError) {
   Sentry.captureException(error);
+  toast.error(i18n.t('errorText'));
   console.error(error);
 }
