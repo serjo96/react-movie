56592521d9559e006294199b43772b01398124dc
serjo96 <promlemboy@gmail.com>
2022-06-15 02:05:16 +0300

    pageview

--- src/utils/initGoogleAds.ts
+++ src/utils/initGoogleAds.ts
@@ -2,5 +2,5 @@ import ReactGA from 'react-ga4';
 
 export default function initGoogleAds () {
   ReactGA.initialize(process.env.GOOGLE_ADS_TRACKING_ID);
-  ReactGA.send(window.location.pathname + window.location.search);
+  ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
 }
