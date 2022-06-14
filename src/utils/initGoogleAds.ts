2bbd151a193a70707c2f2de512a2cbe27f1d147d
serjo96 <promlemboy@gmail.com>
2022-06-15 01:58:37 +0300

    Added send location

--- src/utils/initGoogleAds.ts
+++ src/utils/initGoogleAds.ts
@@ -2,5 +2,5 @@ import ReactGA from 'react-ga4';
 
 export default function initGoogleAds () {
   ReactGA.initialize(process.env.GOOGLE_ADS_TRACKING_ID);
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  ReactGA.send(window.location.pathname + window.location.search);
 }
